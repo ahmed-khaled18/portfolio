b0f0b9564d796afd022bb2b9ce1bc92e6eebe202
Ahmed Khaled <a.k.nasr18@gmail.com>
2025-09-21 19:37:59 +0300

    feat(styles): add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'InterVariable',
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'sans-serif',
+        ],
+        display: ['InterDisplay', 'Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
